b193b3d724d162e335f37244b8011ff7c4e26f6f
Mark Pearyer <marcusp619@gmail.com>
2019-04-26 21:54:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n > 1
+    n -= 1
+    puts "#{n} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  while n > 0
+  sleep(1.5)
+    n -= 1
+    "#{n} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
 end
